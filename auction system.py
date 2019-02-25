3350a35f4a97184b52c56f13c18b603fc483df31
fuxiaofeng <jayfely@qq.com>
2019-02-25 02:07:57 +0000

    multi agent bidding

--- auction system.py
+++ auction system.py
@@ -2,10 +2,11 @@ from __future__ import division
 import csv
 import random
 import matplotlib.pyplot as plt 
+import numpy as np
 
 BUDGET = 6250000
 
-def RTB_simulation(price):
+def RTB_simulation(lower_bound, upper_bound):
 	click_through_rate = 0
 	total_impression = 0
 	total_click = 0
@@ -20,7 +21,7 @@ def RTB_simulation(price):
 
 			click = int(temp[0])
 			payprice =int(temp[21])
-
+			price = np.random.randint(lower_bound, upper_bound)
 			if price > payprice:
 				if total_cost+ payprice <= BUDGET:
 					total_cost += payprice
@@ -45,25 +46,42 @@ def random_bidding_strategy():
 	highest_CTR = 0
 	highest_Click = 0
 
-	for i in xrange(100):
-		lower_bound += 1
-		upper_bound = lower_bound
-		while upper_bound < 300:
-			upper_bound += 10
-			bid = random.randint(lower_bound,upper_bound)
+	agents = []
+	clicks = []
 
-			click, ctr = multi_agent_RTB_simulation(bid)
-			global Highest_Click
-			if click > highest_Click:
-				highest_Click = click
-				highest_CTR = ctr
-				highest_upper_bound = upper_bound
-				highest_lower_bound = lower_bound
 
-		print "Progress:", i
+	for i in range(50,101):
+		click, ctr = multi_agent_RTB_simulation(16, 146, i)
+		agents.append(i)
+		clicks.append(click)
+		print "total clicks:", click, "CTR:", ctr
+		print "progress", i
 
-	print "Best upper bound: ", highest_upper_bound, "Best lower bound: ", highest_lower_bound
-	print "Highest click: ", highest_Click, "Highest_CTR: ", highest_CTR
+	plt.plot(agents, clicks, linewidth=3)
+	# plt.plot(bid_price, clicks, linewidth=3)
+	plt.xlabel('agent number') 
+	plt.ylabel('clicks') 
+	# plt.ylabel('total clicks') 
+	plt.title('Multi Agent Random Bidding') 
+	plt.legend()
+	plt.show() 
+
+	# for i in xrange(100):
+	# 	lower_bound += 1
+	# 	upper_bound = lower_bound
+	# 	while upper_bound < 300:
+	# 		upper_bound += 10
+	# 		click, ctr = multi_agent_RTB_simulation(lower_bound, upper_bound)
+	# 		if click > highest_Click:
+	# 			highest_Click = click
+	# 			highest_CTR = ctr
+	# 			highest_upper_bound = upper_bound
+	# 			highest_lower_bound = lower_bound
+	# 		print "click number", click, "CTR", ctr
+	# 	print "Progress:", i
+
+	# print "Best upper bound: ", highest_upper_bound, "Best lower bound: ", highest_lower_bound
+	# print "Highest click: ", highest_Click, "Highest_CTR: ", highest_CTR
 
 
 def constant_bidding_strategy():
@@ -99,11 +117,12 @@ def constant_bidding_strategy():
 	plt.legend()
 	plt.show() 
 
-def multi_agent_RTB_simulation(price):
+def multi_agent_RTB_simulation(lower_bound, upper_bound, agents_number):
 	click_through_rate = 0
 	total_impression = 0
 	total_click = 0
 	total_cost = 0
+	# agents_number = 50
 
 	with open('we_data/validation.csv', 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -114,19 +133,20 @@ def multi_agent_RTB_simulation(price):
 
 			click = int(temp[0])
 			payprice =int(temp[21])
+			temp_prices = np.random.randint(low=44, high=94, size=agents_number)
 
 			# agents number 
-			agents_number = 50
+			# agents_number = 50
+
 			# Bidding status
 			highest_price = payprice
 			second_price = payprice
-
+			price = np.random.randint(lower_bound, upper_bound)
 			# multi agents
 			for i in range(agents_number):
-				temp_price = random.randint(7, 127)
-				if temp_price > highest_price and temp_price > payprice:
+				if temp_prices[i] > highest_price and temp_prices[i] > payprice:
 					second_price = highest_price
-					highest_price = temp_price
+					highest_price = temp_prices[i]
 
 			if price > highest_price:
 				if total_cost + second_price <= BUDGET:
